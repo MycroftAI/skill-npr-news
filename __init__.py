cb7a1291f3a69d5141d495e9cac69deb7d2d4f1f
penrods <github@gpll.org>
2018-04-10 02:18:35 -0500

    Modernize skill and fix potential URL load issue
    Fix an issue when the remote setting hasn't been downloaded
    when the skill first loads, resulting in a blank news URL.
    Now it loads from the original mycroft.conf URL setting.
    
    Several changes to bring this skill up to modern standards:
    * Use the @intent_handler()
    * Remove the unnecessary 'stop' command implementation
    * Simplify the vocab name
    * Remove the redundant 'news has been stopped' audio
      feedback -- it is pretty obvious it has stopped when
      it stops!
    * Remove now-inappropriate intent tests
    * Move license to Apache 2.0

--- __init__.py
+++ __init__.py
@@ -1,90 +1,75 @@
-# Copyright 2016 Mycroft AI, Inc.
+# Copyright 2018 Mycroft AI Inc.
 #
-# This file is part of Mycroft Core.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import feedparser
-import time
 from os.path import dirname
 import re
 
 from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill
-from mycroft.util.log import getLogger
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.audio import wait_while_speaking
+from mycroft.util.log import LOG
 try:
     from mycroft.skills.audioservice import AudioService
 except:
     from mycroft.util import play_mp3
     AudioService = None
 
-__author__ = 'jdorleans'
-
-LOGGER = getLogger(__name__)
-
 
 class NewsSkill(MycroftSkill):
     def __init__(self):
         super(NewsSkill, self).__init__(name="NewsSkill")
-        self.pre_select=self.settings.get("pre_select")
-        self.url_rss = self.settings.get("url_rss") 
         self.process = None
         self.audioservice = None
 
     def initialize(self):
-        self.pre_select=self.settings.get("pre_select")
+        self.pre_select = self.settings.get("pre_select")
+        self.url_rss = self.settings.get("url_rss")
         if "not_set" in self.pre_select:
-            self.url_rss= self.settings.get("url_rss")
+            # Use a custom RSS URL
+            self.url_rss = self.settings.get("url_rss")
         else:
-            self.url_rss=self.pre_select;
-        intent = IntentBuilder("NewsIntent").require(
-            "NewsKeyword").build()
-        self.register_intent(intent, self.handle_intent)
+            # Use the selected preset's URL
+            self.url_rss = self.pre_select
 
-        intent = IntentBuilder("NewsStopIntent") \
-                .require("NewsStopVerb") \
-                .require("NewsKeyword").build()
-        self.register_intent(intent, self.handle_stop)
+        if not self.url_rss and 'url_rss' in self.config:
+            self.url_rss = self.config['url_rss']
 
         if AudioService:
             self.audioservice = AudioService(self.emitter)
 
+    @intent_handler(IntentBuilder("").require("Play").require("News"))
     def handle_intent(self, message):
         try:
             data = feedparser.parse(self.url_rss)
-            # if news is already playing, stop it silently
+            # Stop anything already playing
             self.stop()
 
             self.speak_dialog('news')
+            wait_while_speaking()
 
-            # Pause for the intro, then start the new stream
-            time.sleep(4)
+            # After the intro, start the news stream
             url = re.sub('https', 'http',
                          data['entries'][0]['links'][0]['href'])
             # if audio service module is available use it
             if self.audioservice:
                 self.audioservice.play(url, message.data['utterance'])
-            else: # othervice use normal mp3 playback
+            else:  # othervice use normal mp3 playback
                 self.process = play_mp3(url)
 
         except Exception as e:
-            LOGGER.error("Error: {0}".format(e))
-
-    def handle_stop(self, message):
-        self.stop()
-        self.speak_dialog('news.stop')
+            LOG.error("Error: {0}".format(e))
 
     def stop(self):
         if self.audioservice:
