b95854f0189131eaa9d210e50b362ad73211d273
Steve Penrod <github@gpll.org>
2019-03-08 07:20:04 +0000

    Add news feed name
    The name of the news feed is now spoken at startup.  Also tweaked the
    news startup sequence to begin speaking while still queuing up, feeling more
    responsive.

--- __init__.py
+++ __init__.py
@@ -29,18 +29,19 @@ STREAM = '/tmp/stream'
 # NOTE: This has to be in synch with the settingsmeta options
 # TODO: Better language support -- this mixes new sources regardless of languages
 FEEDS = {
-    "other" : None,
-    "custom" : None,
-    "BBC" : "http://podcasts.files.bbci.co.uk/p02nq0gn.rss",
-    "NPR" : "http://www.npr.org/rss/podcast.php?id=500005",
-    "AP" : "http://www.spreaker.com/show/1401466/episodes/feed",
-    "CBC" : "http://www.cbc.ca/podcasting/includes/hourlynews.xml",
-    "FOX" : "http://feeds.foxnewsradio.com/FoxNewsRadio",
-    "PBS" : "https://www.pbs.org/newshour/feeds/rss/podcasts/show",
-    "YLE" : "https://feeds.yle.fi/areena/v1/series/1-1440981.rss",
-    "DLF" : "https://www.deutschlandfunk.de/podcast-nachrichten.1257.de.podcast.xml",
-    "WDR" : "https://www1.wdr.de/mediathek/audio/wdr-aktuell-news/wdr-aktuell-152.podcast"
+    "other" : ("your custom feed", None),
+    "custom" : ("your custom feed", None),
+    "BBC" : ("BBC News", "http://podcasts.files.bbci.co.uk/p02nq0gn.rss"),
+    "NPR" : ("NPR News Now", "http://www.npr.org/rss/podcast.php?id=500005"),
+    "AP" :  ("AP Hourly Radio News", "http://www.spreaker.com/show/1401466/episodes/feed"),
+    "CBC" : ("CBC.ca News", "http://www.cbc.ca/podcasting/includes/hourlynews.xml"),
+    "FOX" : ("Fox News", "http://feeds.foxnewsradio.com/FoxNewsRadio"),
+    "PBS" : ("PBS NewsHour", "https://www.pbs.org/newshour/feeds/rss/podcasts/show"),
+    "YLE" : ("YLE", "https://feeds.yle.fi/areena/v1/series/1-1440981.rss"),
+    "DLF" : ("DLF", "https://www.deutschlandfunk.de/podcast-nachrichten.1257.de.podcast.xml"),
+    "WDR" : ("WDR", "https://www1.wdr.de/mediathek/audio/wdr-aktuell-news/wdr-aktuell-152.podcast")
 }
+
 
 def find_mime(url):
     mime = 'audio/mpeg'
@@ -53,6 +54,7 @@ class NewsSkill(CommonPlaySkill):
     def __init__(self):
         super().__init__(name="NewsSkill")
         self.curl = None
+        self.now_playing = None
 
     def CPS_match_query_phrase(self, phrase):
         # Look for a specific news provider
@@ -72,12 +74,12 @@ class NewsSkill(CommonPlaySkill):
     def CPS_start(self, phrase, data):
         if data and "feed" in data:
             # Play the requested news service
-            self.handle_latest_news(rss=FEEDS[data["feed"]])
+            self.handle_latest_news(feed=data["feed"])
         else:
             # Just use the default news feed
-            self.handle_latest_news(None)
+            self.handle_latest_news()
 
-    def get_feed(self, url=None):
+    def get_rss(self, url=None):
         url_rss = url
         if not url_rss:
             pre_select = self.settings.get("pre_select", "")
@@ -85,13 +87,22 @@ class NewsSkill(CommonPlaySkill):
             if "not_set" in pre_select:
                 # Use a custom RSS URL
                 url_rss = self.settings.get("url_rss")
+                self.now_playing = None
             else:
                 # Use the selected preset's URL
                 url_rss = pre_select
-                
-        if not url_rss:
-            url_rss = FEEDS["NPR"]
+                # Look for the title
+                for feed in FEEDS:
+                    if FEEDS[feed][1] == url_rss:
+                        self.now_playing = FEEDS[feed][0]
 
+        if not url_rss:
+            # Default to NPR News
+            self.now_playing = FEEDS["NPR"][0]
+            url_rss = FEEDS["NPR"][1]
+        return url_rss
+
+    def get_feed(self, url_rss):
         data = feedparser.parse(url_rss.strip())
         # After the intro, find and start the news stream
         # select the first link to an audio file
@@ -102,7 +113,7 @@ class NewsSkill(CommonPlaySkill):
         else:
             # fall back to using the first link in the entry
             media = data['entries'][0]['links'][0]['href']
-        self.log.info('Will play news from URL: '+media)
+        self.log.info('Playing news from URL: '+media)
         return media
 
     @intent_file_handler("PlayTheNews.intent")
@@ -111,24 +122,32 @@ class NewsSkill(CommonPlaySkill):
         utt = message.data["utterance"]
         match = self.CPS_match_query_phrase(utt)
         if match and len(match) > 2:
-            self.log.info("Match: " + str(match))
-            rss = FEEDS[match[2]["feed"]]
+            feed = match[2]["feed"]
         else:
-            rss = None
+            feed = None
 
-        self.handle_latest_news(message, rss)
+        self.handle_latest_news(message, feed)
 
     @intent_handler(IntentBuilder("").require("Latest").require("News"))
-    def handle_latest_news(self, message=None, rss=None):
+    def handle_latest_news(self, message=None, feed=None):
         try:
             self.stop()
+
+            rss = None
+            self.now_playing = None
+            if feed and feed in FEEDS:
+                self.now_playing = FEEDS[feed][0]
+                rss = FEEDS[feed][1]
+
+            # Speak intro while downloading in background
+            rss = self.get_rss(rss)
+            self.speak_dialog('news', data={"from": self.now_playing})
 
             url = self.get_feed(rss)
             mime = find_mime(url)
             # (Re)create Fifo
             if os.path.exists(STREAM):
                 os.remove(STREAM)
-            self.log.debug('Creating fifo')
             os.mkfifo(STREAM)
 
             self.log.debug('Running curl {}'.format(url))
@@ -136,8 +155,8 @@ class NewsSkill(CommonPlaySkill):
                 'curl -L "{}" > {}'.format(url, STREAM),
                 shell=True)
 
-            # Speak an intro
-            self.speak_dialog('news', wait=True)
+            # Show news title, if there is one
+            wait_while_speaking()
             # Begin the news stream
             self.CPS_play(('file://' + STREAM, mime))
 
@@ -147,7 +166,7 @@ class NewsSkill(CommonPlaySkill):
             self.speak_dialog("could.not.start.the.news.feed")
 
     def stop(self):
-        """ Stop download process if it's running. """
+        # Stop download process if it's running.
         if self.curl:
             try:
                 self.curl.kill()
