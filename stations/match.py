4186881cc9f42c53af21f04f3cbd2342a1211ca3
Kris Gesling <kris.gesling@mycroft.ai>
2021-08-23 23:04:54 +0930

    Fix and simplify station matching

--- stations/match.py
+++ stations/match.py
@@ -14,6 +14,7 @@
 
 from collections import namedtuple
 
+from mycroft.util import LOG
 from mycroft.util.parse import fuzzy_match
 
 from . import stations
@@ -21,6 +22,7 @@ from . import stations
 
 # Minimum confidence levels
 CONF_EXACT_MATCH = 0.9
+CONF_HIGH_MATCH = 0.8
 CONF_LIKELY_MATCH = 0.7
 CONF_GENERIC_MATCH = 0.6
 
@@ -40,25 +42,27 @@ def match_station_name(phrase, station, aliases, news_keyword):
         tuple: feed being matched, highest confidence level found
     """
     phrase = phrase.lower().replace("play", "").strip()
-    station_acronym = station.acronym.lower()
-    # Test with "News" added in case user only says acronym eg "ABC".
-    # As it is short it may not provide a high match confidence.
-    modified_short_name = "{} {}".format(station_acronym, news_keyword)
 
     match_confidences = [
-        fuzzy_match(phrase, station_acronym),
+        # fuzzy_match(phrase, station.acronym.lower()),
         fuzzy_match(phrase, station.full_name.lower()),
-        fuzzy_match(phrase, modified_short_name),
     ]
 
     # Check aliases defined in alt.feed.name.value
     if aliases:
         match_confidences += [fuzzy_match(phrase, alias) for alias in aliases]
 
-    # If phrase contains both the news keyword and the station name ensure a
-    # minimum confidence of a generic match.
-    if news_keyword in phrase:
-        if station_acronym in phrase:
+    # If phrase contains both a station acronym and the news keyword
+    # ensure minimum confidences.
+    if f"{station.acronym.lower()} {news_keyword}" in phrase:
+        # Eg "play DLF News"
+        match_confidences.append(CONF_HIGH_MATCH)
+    if station.acronym.lower() in phrase:
+        if news_keyword in phrase:
+            # Eg "what's the news on DLF"
+            match_confidences.append(CONF_LIKELY_MATCH)
+        else:
+            # Eg "what's on DLF"
             match_confidences.append(CONF_GENERIC_MATCH)
 
     highest_confidence = max(match_confidences)
@@ -82,17 +86,23 @@ def match_station_from_utterance(skill, utterance):
         # User is not asking for the news - do not match.
         return match
     
+    # If news vocab does exist, provide a minimum default confidence
+    default_station = skill.get_default_station()
+    match = Match(default_station, CONF_GENERIC_MATCH)
+
     # Catch any short explicit phrases eg 'play the news'
     news_phrases = skill.translate_list("PlayTheNews") or []
     if utterance in news_phrases:
-        station = skill.get_default_station()
-        match = Match(station, 1.0)
+        LOG.debug("Explicit phrase without specific station detected.")
+        return Match(default_station, 1.0)
 
     # Test against each station to find the best match.
     news_keyword = skill.translate('OnlyNews').lower()
+    LOG.debug("Matching against specific stations")
     for station in stations.values():
         aliases = skill.alternate_station_names.get(station.acronym)
         station_match = match_station_name(utterance, station, aliases, news_keyword)
+        LOG.debug(f"{station.acronym}: {match.confidence}")
         if station_match.confidence > match.confidence:
             match = station_match
 
