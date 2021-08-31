ab9faf383db85aa5497d86c4be42e7910422719e
Kris Gesling <kris.gesling@mycroft.ai>
2021-08-31 13:36:20 +0930

    Clean up imports

--- stations/gpb.py
+++ stations/gpb.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import feedparser
 import re
+
+import feedparser
 import requests
 
 def get_gpb_url():
